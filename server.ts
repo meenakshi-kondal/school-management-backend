1f58368a8a7ab09134dbeefd5de34e254e40a89c
Meenakshi <meenakshi.devi@jupitice.com>
2025-09-29 11:20:28 +0530

    added auth apis

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import connectDB from "./src/config/db";
 import allRoutes from "./src/routes/allRoutes";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
