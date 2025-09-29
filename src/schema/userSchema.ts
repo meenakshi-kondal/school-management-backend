4e351e2cdf5cedc91651d4d86e726eb68eca3fcd
Meenakshi <meenakshi.devi@jupitice.com>
2025-09-29 12:03:23 +0530

    added schema for qualification

--- src/schema/userSchema.ts
+++ src/schema/userSchema.ts
@@ -5,13 +5,20 @@ const classSchema = new mongoose.Schema({
   subjects: [{ type: String }],
 });
 
+const qualificationSchema = new mongoose.Schema({
+  institue_name: { type: String, required: true },
+  degree: { type: String },
+  year: { type: Date, required: true },
+  board: { type: String },
+});
+
 const user = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   gender: { type: String, enum: ["male", "female", "other"], required: true },
   dateOfBirth: { type: Date, required: true },
   role: { type: String, enum: ["teacher", "student", "admin"], default: "teacher" },
-  qualification: { type: String },
+  qualification: [qualificationSchema],
   class: [classSchema],
   joiningDate: { type: Date, required: true },
   is_bus_service: { type: Boolean, default: false },
